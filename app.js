b6146c661cb678ff85daa553cac38089ad180623
최철훈 <2015005187@hanyang.ac.kr>
2021-02-08 19:49:53 +0900

    tutorial

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
 const express = require('express');
-
+var bodyParser = require('body-parser');
 const app = express();
 app.locals.pretty = true;
 app.set('view engine', 'jade');
 app.set('views', './views');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.urlencoded({extended:true}))
+app.get('/form', function(req, res){
+    res.render('form');
+})
+app.get('/form_receiver', function(req, res){
+    var title = req.query.title;
+    var description = req.query.description;
+    res.send(title+', '+description);
+})
+app.post('/form_receiver', function(req, res){
+    var title = req.body.title;
+    var description = req.body.description;
+    res.send(title+', '+description);
+})
+app.get('/topic/:id', function(req, res){
+    var topics = ['Javascript is...', 'Nodejs is...', 'Express is...'];
+    var output = `
+    <a href="/topic/0">JavaScript</a><br>
+    <a href="/topic/1">Nodejs</a><br>
+    <a href="/topic/2">Express</a><br><br>
+    ${topics[req.params.id]}
+    `
+    res.send(output);
+})
+app.get('/topic/:id/:mode', function(req, res){
+    res.send(req.params.id + ' ' + req.params.mode)
+})
 app.get('/template', function(req, res){
     res.render('temp', {time:Date(), _title:'Jade'});
 })
